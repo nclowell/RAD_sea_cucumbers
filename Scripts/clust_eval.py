460399f07f4fb9114138054066ff6d1a597262ed
nclowell <nclowell@uw.edu>
2017-08-10 10:33:00 -0700

    updated plots for NB, updated NB, updated script, clust eval

--- Scripts/clust_eval.py
+++ Scripts/clust_eval.py
@@ -1,5 +1,6 @@
-# 20170807 Natalie Lowell
-# Clustering similarity evaluation + plots
+###### Clustering similarity evaluation + plots #######
+# PURPOSE: make plots to help user evaluate effect of clustering
+# similarity parameter value on genotypes in ipyrad
 # INPUTS: managed by argparse, include:
 # - path to ipyrad directory
 # - text file with all assembly names, each on own line
@@ -8,15 +9,17 @@
 # OUTPUTS: plots comparing many output metrics to help evaluate
 # the effect of clustering similarity
 ######################################################
+
+# import modules
 from __future__ import division
 import os
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+# manage command line arguments
 parser = argparse.ArgumentParser(description="Read in step 7 stats files, plot contents to help user evaluate effect of clustering similarity")
-parser.add_argument("-d", "--dir", help="path to ipyrad directory, without final slash", type=str, required = True)
+parser.add_argument("-d", "--dir", help="path to ipyrad directory", type=str, required = True)
 parser.add_argument("-n", "--names", help="text file of assembly names, each on own line, stored in ipyrad dir", type=str, required = True)
 parser.add_argument("-c", "--clusts", help="text file of clustering similarity parameter values, each on own line, and matching order of assembly names text file, stored in ipyrad dir", type=str, required = True)
 args = parser.parse_args()
@@ -32,7 +35,7 @@ for line in assemblies_file:
     if name != "":
         assemblies.append(name)
 
-# get clusts out of input file
+# get clustering similarity parameter values out of input file
 clusts_file = open(args.clusts,"r")
 clusts = []
 for line in clusts_file:
@@ -50,7 +53,7 @@ tot_prefilt_loci = []
 ret_loci_after_minsample = []
 applied_order_MA = []
 
-# iterate through assembles, extract information, and store in lists
+# iterate through assembles, extract information from stats files, and store in lists
 for assembly in assemblies:
     stats_file = open(assembly + "_outfiles/" + assembly + "_stats.txt", "r")
     stats_file_lines = stats_file.readlines()
@@ -80,26 +83,26 @@ plt.ylabel("var = 0 / total filtered loci")
 plt.savefig("clust_eval_plots/prop_hom_wsingletons.png")
 plt.close()
 
-# proportion homozygous loci from retained_loci
+# proportion homozygous loci without singletons
 plt.plot(clusts,[x/y for x, y in zip(pi_0, ret_loci_after_minsample)])
 plt.title("Proportion of loci homozygous without singletons")
 plt.xlabel("Clustering similarity %")
-plt.ylabel("pi = 0")
+plt.ylabel("pi = 0 / retained loci after minimum sample")
 plt.savefig("clust_eval_plots/prop_hom_wosingletons.png")
 plt.close()
 
 # proportion heterozygous loci
 props_het = []
-for i in [x/y for x, y in zip(pi_0, ret_loci_after_minsample)]:
+for i in [(y-x)/y for x, y in zip(pi_0, tot_filt_loci)]:
     props_het.append(1-i)
 plt.plot(clusts,props_het)
 plt.title("Proportion of heterozygous loci")
 plt.xlabel("Clustering similarity %")
-plt.ylabel("1 - proportion homozygous, after filter for max alleles = 2")
+plt.ylabel("(total filtered loci - pi_0)/total filtered loci")
 plt.savefig("clust_eval_plots/prop_het.png")
 plt.close()
 
-# proportion filtered by max alleles = 2
+# proportion filtered out for polyploidy (max alleles = 2)
 plt.plot(clusts,[x/y for x, y in zip(applied_order_MA, ret_loci_after_minsample)])
 plt.title("Proportion loci filtered out for polyploidy")
 plt.xlabel("Clustering similarity %")
@@ -107,10 +110,12 @@ plt.ylabel("Applied_order filtered max alleles / retained_loci after min sample"
 plt.savefig("clust_eval_plots/prop_lost_polyploids.png")
 plt.close()
 
-# proportion loci lost to remove duplicates
+# proportion loci lost to remove duplicates (potentially over clustering)
 plt.plot(clusts,[x/y for x, y in zip(total_filters_RD, tot_prefilt_loci)])
 plt.title("Proportion of loci lost to remove_duplicates")
 plt.xlabel("Clustering similarity %")
 plt.ylabel("total_filters remove duplicates / total prefiltered loci")
 plt.savefig("clust_eval_plots/prop_lost_remdups.png")
 plt.close()
+
+# 20170810 Natalie Lowell
